c01c31ecfe17aeddd5c3f01ada0c63bdc8c6edae
kim <yupyong.kim@gmail.com>
2024-03-15 08:26:57 +0900

    create:ApiClient

---
+++ lib/service/api/api_client.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'api_client.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$pokemonApiClientHash() => r'84ff7ebc486196db3858fbc3fd0698fba825432d';
+
+/// See also [pokemonApiClient].
+@ProviderFor(pokemonApiClient)
+final pokemonApiClientProvider = AutoDisposeProvider<ApiClient>.internal(
+  pokemonApiClient,
+  name: r'pokemonApiClientProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$pokemonApiClientHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef PokemonApiClientRef = AutoDisposeProviderRef<ApiClient>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
