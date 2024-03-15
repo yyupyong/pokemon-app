00372ea8cda83a9f3abfffa738819ed130136522
kim <yupyong.kim@gmail.com>
2024-03-15 21:52:22 +0900

    update:back ground UI

--- lib/feature/base/base_screen.dart
+++ lib/feature/base/base_screen.dart
@@ -6,14 +6,40 @@ class BaseScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pokemon app'),
-        backgroundColor: Colors.amber,
-        actions: [
-          IconButton(
-            color: Colors.red,
-            onPressed: () {},
-            icon: const Icon(Icons.favorite),
+      body: Stack(
+        children: [
+          Positioned(
+            right: -24,
+            child: Image.asset(
+              'assets/MonsterBall_gray.png',
+              height: 160,
+              width: 160,
+            ),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).padding.top + 24,
+            left: 16,
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Pokemon',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w800),
+                ),
+              ],
+            ),
+          ),
+          Positioned.fill(
+            top: MediaQuery.of(context).padding.top + 24,
+            left: 16,
+            child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (ctx, i) {
+                return Text(
+                  i.toString(),
+                );
+              },
+            ),
           ),
         ],
       ),
