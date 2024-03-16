a924e6a82e26e8ac97f1d8fd349e47fc1b5623bb
kim <yupyong.kim@gmail.com>
2024-03-16 21:46:25 +0900

    update:base screen

--- lib/feature/base/base_screen.dart
+++ lib/feature/base/base_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pokemon_app/feature/base/pokemon_fetch.dart';
 import 'package:pokemon_app/gen/assets.gen.dart';
 
-class BaseScreen extends StatelessWidget {
+class BaseScreen extends ConsumerWidget {
   const BaseScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final pokemonList = ref.watch(fetchPokemonListProvider);
+
     return Scaffold(
       body: Stack(
         children: [
@@ -14,37 +18,47 @@ class BaseScreen extends StatelessWidget {
             child: Assets.monsterBallGray
                 .image(width: 160, height: 160, fit: BoxFit.cover),
           ),
-          CustomScrollView(
-            slivers: [
-              SliverPadding(
-                padding: EdgeInsets.only(
-                  top: MediaQuery.of(context).padding.top + 24,
-                  left: 16,
-                ),
-                sliver: const SliverToBoxAdapter(
-                  child: Text(
-                    'Pokemon',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.w800),
+          pokemonList.maybeWhen(
+            orElse: () => const Center(
+              child: CircularProgressIndicator(),
+            ),
+            data: (data) {
+              return CustomScrollView(
+                slivers: [
+                  SliverPadding(
+                    padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).padding.top + 24,
+                      left: 16,
+                    ),
+                    sliver: const SliverToBoxAdapter(
+                      child: Text(
+                        'Pokemon',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              SliverPadding(
-                padding: const EdgeInsets.only(
-                  top: 24,
-                  left: 16,
-                ),
-                sliver: SliverList(
-                  delegate: SliverChildBuilderDelegate(
-                    childCount: 10,
-                    (context, index) {
-                      return Text(
-                        index.toString(),
-                      );
-                    },
+                  SliverPadding(
+                    padding: const EdgeInsets.only(
+                      top: 24,
+                      left: 16,
+                    ),
+                    sliver: SliverList(
+                      delegate: SliverChildBuilderDelegate(
+                        childCount: data.items.length,
+                        (context, index) {
+                          return Text(
+                            data.items[index].name,
+                          );
+                        },
+                      ),
+                    ),
                   ),
-                ),
-              ),
-            ],
+                ],
+              );
+            },
           ),
         ],
       ),
