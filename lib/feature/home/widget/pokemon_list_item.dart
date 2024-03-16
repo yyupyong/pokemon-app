0dc073f5959bae63768a9e8790132e72fda197a8
kim <yupyong.kim@gmail.com>
2024-03-17 00:46:22 +0900

    update: PokemonListItem widget

--- lib/feature/home/widget/pokemon_list_item.dart
+++ lib/feature/home/widget/pokemon_list_item.dart
@@ -1,46 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pokemon_app/const/pokeapi_color.dart';
+import 'package:pokemon_app/feature/detail/pokemon_detail_screen.dart';
 import 'package:pokemon_app/feature/home/model/pokemon_detail.dart';
 
 class PokemonListItem extends ConsumerWidget {
   const PokemonListItem({super.key, required this.poke});
   final PokemonDetail? poke;
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    if (poke != null) {
-      return ListTile(
-        leading: Container(
-          width: 80,
-          decoration: BoxDecoration(
-            color: (pokeTypeColors[poke!.types.first.type.name] ??
-                    Colors.grey[100])
-                ?.withOpacity(.3),
-            borderRadius: BorderRadius.circular(10),
-            image: DecorationImage(
-              fit: BoxFit.fitWidth,
-              image: NetworkImage(
-                poke!.sprites.other.officialArtwork.frontDefault,
-              ),
+    if (poke == null) {
+      return const ListTile(title: Text('...'));
+    }
+
+    final nonNullPoke = poke!;
+
+    return ListTile(
+      leading: Container(
+        width: 80,
+        decoration: BoxDecoration(
+          color: (pokeTypeColors[nonNullPoke.types.first.type.name] ??
+                  Colors.grey[100])
+              ?.withOpacity(.3),
+          borderRadius: BorderRadius.circular(10),
+          image: DecorationImage(
+            fit: BoxFit.fitWidth,
+            image: NetworkImage(
+              nonNullPoke.sprites.other.officialArtwork.frontDefault,
             ),
           ),
         ),
-        title: Text(
-          poke!.name,
-          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-        ),
-        subtitle: Text(poke!.types.first.type.name),
-        trailing: const Icon(Icons.navigate_next),
-        onTap: () => {
-          // Navigator.of(context).push(
-          //   MaterialPageRoute(
-          //     builder: (BuildContext context) => const PokeDetail(),
-          //   ),
-          // ),
-        },
-      );
-    } else {
-      return const ListTile(title: Text('...'));
-    }
+      ),
+      title: Text(
+        nonNullPoke.name,
+        style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+      ),
+      subtitle: Text(nonNullPoke.types.first.type.name),
+      trailing: const Icon(Icons.navigate_next),
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (BuildContext context) => PokemonDetailScreen(
+              poke: nonNullPoke,
+            ),
+          ),
+        );
+      },
+    );
   }
 }
