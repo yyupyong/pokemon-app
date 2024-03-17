a73093fbbbc5107a5ae28beec188a5fcf669aab3
kim <yupyong.kim@gmail.com>
2024-03-17 11:42:06 +0900

    update: detail page UI

--- lib/feature/detail/pokemon_detail_screen.dart
+++ lib/feature/detail/pokemon_detail_screen.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:pokemon_app/const/pokeapi_color.dart';
 import 'package:pokemon_app/feature/home/model/pokemon_detail.dart';
+import 'package:pokemon_app/feature/home/widget/type_tag.dart';
 import 'package:pokemon_app/gen/assets.gen.dart';
 
 class PokemonDetailScreen extends StatelessWidget {
@@ -10,77 +13,128 @@ class PokemonDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: [
-          Positioned.fill(
-            child: Container(
-              decoration: BoxDecoration(
-                color: (pokeTypeColors[poke.types.first.type.name] ??
-                        Colors.grey[100])
-                    ?.withOpacity(.5),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        body: Stack(
+          fit: StackFit.expand,
+          children: [
+            Positioned.fill(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: (pokeTypeColors[poke.types.first.type.name] ??
+                          Colors.grey[100])
+                      ?.withOpacity(.6),
+                ),
               ),
             ),
-          ),
-          Positioned(
-            right: -24,
-            child: Assets.monsterBallWhite
-                .image(width: 160, height: 160, fit: BoxFit.cover),
-          ),
-          Positioned(
-            top: MediaQuery.of(context).padding.top,
-            left: 16,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                GestureDetector(
-                  behavior: HitTestBehavior.opaque,
-                  onTap: () => Navigator.pop(context),
-                  child: const Icon(
-                    Icons.arrow_back,
-                    size: 32,
-                    color: Colors.white,
-                  ),
-                ),
-                const SizedBox(height: 16),
-                Text(
-                  poke.name,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                    fontWeight: FontWeight.w800,
-                  ),
-                ),
-              ],
+            Positioned(
+              right: -24,
+              child: Assets.monsterBallWhite
+                  .image(width: 180, height: 180, fit: BoxFit.cover),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 160),
-            child: Column(
-              children: [
-                Image.network(
-                  poke.sprites.other.officialArtwork.frontDefault,
-                  height: 160,
-                  width: 160,
-                ),
-                const SizedBox(height: 24),
-                Expanded(
-                  child: Container(
-                    decoration: const BoxDecoration(
+            Positioned(
+              top: MediaQuery.of(context).padding.top,
+              left: 16,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  GestureDetector(
+                    behavior: HitTestBehavior.opaque,
+                    onTap: () => Navigator.pop(context),
+                    child: const Icon(
+                      Icons.arrow_back,
+                      size: 32,
                       color: Colors.white,
-                      borderRadius: BorderRadius.vertical(
-                        top: Radius.circular(24),
-                      ),
                     ),
-                    width: double.infinity,
-                    child: const Column(),
                   ),
-                ),
-              ],
+                  const SizedBox(height: 16),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.baseline,
+                        textBaseline: TextBaseline.alphabetic,
+                        children: [
+                          Text(
+                            poke.name,
+                            style: const TextStyle(
+                              fontSize: 28,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(width: 4),
+                          Text(
+                            '#${poke.id}',
+                            style: const TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 12),
+                      TypeTag(typeList: poke.types),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.only(top: 200),
+              child: Column(
+                children: [
+                  Image.network(
+                    poke.sprites.other.officialArtwork.frontDefault,
+                    height: 260,
+                    width: 260,
+                  ),
+                  const SizedBox(height: 24),
+                  const TabBar(
+                    labelStyle: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    dividerColor: Colors.transparent,
+                    indicatorColor: Colors.white,
+                    indicatorWeight: 5,
+                    tabs: [
+                      Tab(
+                        text: 'Details',
+                      ),
+                      Tab(
+                        text: 'Status',
+                      ),
+                    ],
+                  ),
+                  const Expanded(
+                    child: TabBarView(
+                      children: [
+                        SingleChildScrollView(
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 16,
+                              vertical: 24,
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                            ),
+                          ),
+                        ),
+                        SingleChildScrollView(
+                          child: Row(),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
