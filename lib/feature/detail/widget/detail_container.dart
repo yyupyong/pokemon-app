93de8e25c0717a1ab725d57f3b9fe8714ae71deb
kim <yupyong.kim@gmail.com>
2024-03-17 19:10:34 +0900

    身長・体重などを表示するContainerをwidgetとして切り出し

---
+++ lib/feature/detail/widget/detail_container.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class DetailContainer extends StatelessWidget {
+  const DetailContainer({super.key, required this.label, required this.value});
+
+  final String label;
+  final String value;
+
+  @override
+  Widget build(BuildContext context) {
+    var displayValue = value;
+    var unit = '';
+
+    if (label.toLowerCase() == 'weight') {
+      final weight = double.parse(value);
+      unit = 'kg';
+      displayValue = (weight / 10).toStringAsFixed(1);
+    } else if (label.toLowerCase() == 'height') {
+      final height = double.parse(value);
+      unit = 'cm';
+      displayValue = (height * 10).toStringAsFixed(0);
+    }
+
+    return Column(
+      children: [
+        Text(
+          label,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        Text(
+          '$displayValue $unit',
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+            color: Colors.white,
+          ),
+        ),
+      ],
+    );
+  }
+}
