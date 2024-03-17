281057cdd924c552a2a52669b20a6a80c47470ac
kim <yupyong.kim@gmail.com>
2024-03-17 10:10:59 +0900

    create: pokemon type tag

---
+++ lib/feature/home/widget/type_tag.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:pokemon_app/const/pokeapi_color.dart';
+import 'package:pokemon_app/feature/home/model/pokemon_detail.dart';
+
+class TypeTag extends StatelessWidget {
+  const TypeTag({super.key, required this.typeList});
+
+  final List<PokemonType> typeList;
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      spacing: 8,
+      runSpacing: 4,
+      children: typeList.map((e) {
+        return Container(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+          decoration: BoxDecoration(
+            color: pokeTypeColors[e.type.name] ?? Colors.grey.shade200,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Text(
+            e.type.name,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 14,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        );
+      }).toList(),
+    );
+  }
+}
