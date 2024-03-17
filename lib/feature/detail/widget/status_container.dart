0be4c40caa4e1d685c43592b61ca4e8dbbc31718
kim <yupyong.kim@gmail.com>
2024-03-17 19:47:54 +0900

    update: Status Container UI

--- lib/feature/detail/widget/status_container.dart
+++ lib/feature/detail/widget/status_container.dart
@@ -2,21 +2,47 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class StatsContainer extends StatelessWidget {
-  const StatsContainer({super.key});
+  const StatsContainer({
+    super.key,
+    required this.label,
+    required this.statusValue,
+  });
+
+  final String label;
+  final String statusValue;
 
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
-    final containerWidth = (screenWidth - 48 - 12) / 2;
+    final containerWidth = (screenWidth - 48 - 16) / 2;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const Text('heigh'),
+        Text(
+          label,
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+            color: Colors.white,
+          ),
+        ),
+        const SizedBox(height: 4),
         Container(
+          padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 4),
           width: containerWidth,
-          decoration: BoxDecoration(border: Border.all()),
-          child: const Center(
-            child: Text('dddd'),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            border: Border.all(color: Colors.white),
+          ),
+          child: Center(
+            child: Text(
+              statusValue,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+              ),
+            ),
           ),
         ),
       ],
