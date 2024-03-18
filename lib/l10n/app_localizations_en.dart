807f1dd354cb88cff44f03aeadeaf34aef109940
kim <yupyong.kim@gmail.com>
2024-03-18 11:26:18 +0900

    generate: AppLocalizations class & file

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,33 @@
+import 'app_localizations.dart';
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get appTitle => 'Pokemon';
+
+  @override
+  String get height => 'Height';
+
+  @override
+  String get weight => 'Weight';
+
+  @override
+  String get hp => 'HP';
+
+  @override
+  String get attack => 'Attack';
+
+  @override
+  String get defence => 'Defence';
+
+  @override
+  String get speed => 'Speed';
+
+  @override
+  String get special_attack => 'Special Attack';
+
+  @override
+  String get special_defence => 'Special Defence';
+}
---
+++ lib/l10n/app_localizations_ja.dart
@@ -0,0 +1,33 @@
+import 'app_localizations.dart';
+
+/// The translations for Japanese (`ja`).
+class AppLocalizationsJa extends AppLocalizations {
+  AppLocalizationsJa([String locale = 'ja']) : super(locale);
+
+  @override
+  String get appTitle => 'ポケモン';
+
+  @override
+  String get height => 'たかさ';
+
+  @override
+  String get weight => 'おもさ';
+
+  @override
+  String get hp => 'HP';
+
+  @override
+  String get attack => 'こうげき';
+
+  @override
+  String get defence => 'しゅび';
+
+  @override
+  String get speed => 'すばやさ';
+
+  @override
+  String get special_attack => 'Special Attack';
+
+  @override
+  String get special_defence => 'Special Defence';
+}
