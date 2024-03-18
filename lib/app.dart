1ad147d50c12705f7f0d7815bfb8ebe12a08721f
kim <yupyong.kim@gmail.com>
2024-03-18 11:26:41 +0900

    update: material widget

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pokemon_app/feature/home/home_screen.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:pokemon_app/l10n/app_localizations.dart';
 
 class App extends ConsumerWidget {
   const App({super.key});
@@ -10,14 +11,12 @@ class App extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       localizationsDelegates: const [
-        GlobalCupertinoLocalizations.delegate,
+        AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      supportedLocales: const [
-        Locale('en'),
-        Locale('ja'),
-      ],
+      supportedLocales: AppLocalizations.supportedLocales,
       debugShowCheckedModeBanner: false,
       title: 'pokemon-app',
       theme: ThemeData(
